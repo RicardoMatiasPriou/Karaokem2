8ffa385f872964c520c51b49febdf6918aa41855
RicardoMatiasPriou <74669252+RicardoMatiasPriou@users.noreply.github.com>
2021-01-07 17:22:24 -0300

    00.00.05

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+//// filtro de repetidos////
+Array.prototype.unique = function (a) {
+  return function () {
+      return this.filter(a)
+  }
+}(function (a, b, c) {
+  return c.indexOf(a, b + 1) < 0
+})
 var firebaseConfig = {
     apiKey: "AIzaSyB3KhN4IO-eFSnW0-R5YCrjPY5iA1Xc-Yk",
     authDomain: "proyecto-final-71ed2.firebaseapp.com",
@@ -8,27 +16,31 @@ var firebaseConfig = {
     appId: "1:551788436915:web:ab3dfe2a1ed487953dffc6",
     measurementId: "G-FT6G73QBF7"
 };
+//// INICIAR FIREBASE Y ANALYTICS /////
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 const db = firebase.firestore();
+
 var comida;
-var categoria1;
-var categoria2;
+var categoria1 ;
+var categoria2 ;
 var precioXUnidad;
 var precioXDocena;
 var img;
+var arrayDeCategoria1 = []
+var arrayDeCategoria2 = []
 const getComidas = () => db.collection('Productos').get();
 window.addEventListener('DOMContentLoaded', async (e) => {
     querySnapshot = await getComidas();
     querySnapshot.forEach(doc => {
-        var contenedorDeProdutcotos = $('#productos')
-        var comida = doc.data().name
-        var categoria1 = doc.data().categoria1
-        var categoria2 = doc.data().categoria2
-        var precioXUnidad = doc.data().precioXUnidad
-        var precioXDocena = doc.data().precioXDocena
-        var img = doc.data().img
-        var texXunidad = `
+        contenedorDeProdutcotos = $('#productos')
+        comida = doc.data().name
+        categoria1 = doc.data().categoria1
+        categoria2 = doc.data().categoria2
+        precioXUnidad = doc.data().precioXUnidad
+        precioXDocena = doc.data().precioXDocena
+        img = doc.data().img
+        texXunidad = `
         <div class="col-lg-4 col-md-6 mb-4">
         <div class="card h-100">
           <a href="#"><img class="card-img-top" src="${img}" alt="img"></a>
@@ -44,6 +56,21 @@ window.addEventListener('DOMContentLoaded', async (e) => {
         </div>
       </div>
       `;
+      arrayDeCategoria1.push(categoria1)
+      arrayDeCategoria2.push(categoria2)
     contenedorDeProdutcotos.append(texXunidad);
     })
-})
+    arrayDeCategoria2.unique().forEach(e =>{
+        $('#categorias').append(`<a href="Agregar productos a base de datos(soloComidas).html" class="list-group-item">${e}</a>
+        `);
+    })
+
+
+    console.log(comida
+      ,categoria1
+      ,categoria2
+      ,precioXUnidad
+      ,precioXDocena
+      ,img,arrayDeCategoria2.unique(),$('#categorias')
+      )
+  })
