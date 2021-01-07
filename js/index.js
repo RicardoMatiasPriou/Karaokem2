6ac555daf0ea1cda8dddba09310740131423749b
RicardoMatiasPriou <74669252+RicardoMatiasPriou@users.noreply.github.com>
2021-01-07 18:41:36 -0300

    00.00.06

--- js/index.js
+++ js/index.js
@@ -1,46 +1,99 @@
 //// filtro de repetidos////
-Array.prototype.unique = function (a) {
+Array.prototype.unique = (function (a) {
   return function () {
-      return this.filter(a)
-  }
-}(function (a, b, c) {
-  return c.indexOf(a, b + 1) < 0
-})
+    return this.filter(a);
+  };
+})(function (a, b, c) {
+  return c.indexOf(a, b + 1) < 0;
+});
 var firebaseConfig = {
-    apiKey: "AIzaSyB3KhN4IO-eFSnW0-R5YCrjPY5iA1Xc-Yk",
-    authDomain: "proyecto-final-71ed2.firebaseapp.com",
-    databaseURL: "https://proyecto-final-71ed2.firebaseio.com",
-    projectId: "proyecto-final-71ed2",
-    storageBucket: "proyecto-final-71ed2.appspot.com",
-    messagingSenderId: "551788436915",
-    appId: "1:551788436915:web:ab3dfe2a1ed487953dffc6",
-    measurementId: "G-FT6G73QBF7"
+  apiKey: "AIzaSyB3KhN4IO-eFSnW0-R5YCrjPY5iA1Xc-Yk",
+  authDomain: "proyecto-final-71ed2.firebaseapp.com",
+  databaseURL: "https://proyecto-final-71ed2.firebaseio.com",
+  projectId: "proyecto-final-71ed2",
+  storageBucket: "proyecto-final-71ed2.appspot.com",
+  messagingSenderId: "551788436915",
+  appId: "1:551788436915:web:ab3dfe2a1ed487953dffc6",
+  measurementId: "G-FT6G73QBF7",
 };
 //// INICIAR FIREBASE Y ANALYTICS /////
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 const db = firebase.firestore();
 
-var comida;
-var categoria1 ;
-var categoria2 ;
-var precioXUnidad;
-var precioXDocena;
-var img;
-var arrayDeCategoria1 = []
-var arrayDeCategoria2 = []
-const getComidas = () => db.collection('Productos').get();
-window.addEventListener('DOMContentLoaded', async (e) => {
-    querySnapshot = await getComidas();
-    querySnapshot.forEach(doc => {
-        contenedorDeProdutcotos = $('#productos')
-        comida = doc.data().name
-        categoria1 = doc.data().categoria1
-        categoria2 = doc.data().categoria2
-        precioXUnidad = doc.data().precioXUnidad
-        precioXDocena = doc.data().precioXDocena
-        img = doc.data().img
-        texXunidad = `
+var arrayDeCategoria1 = [];
+var arrayDeCategoria2 = [];
+var querySnapshot;
+var categoriaseleccionada
+const getComidas = () => db.collection("Productos").get();
+window.addEventListener("DOMContentLoaded", async (e) => {
+  querySnapshot = await getComidas();
+  CrearCajasDeProductos();
+  FiltrosDeCategoria();
+
+
+});
+
+function CrearCajasDeProductos() {
+  querySnapshot.forEach((doc) => {
+    contenedorDeProdutcotos = $("#productos");
+    var comida = doc.data().name;
+    var precioXUnidad = doc.data().precioXUnidad;
+    var precioXDocena = doc.data().precioXDocena;
+    var img = doc.data().img;
+    texXunidad = `
+    <div class="col-lg-4 col-md-6 mb-4">
+    <div class="card h-100">
+      <a href="#"><img class="card-img-top" src="${img}" alt="img"></a>
+      <div class="card-body">
+        <h4 class="card-title">
+          <a href="#">${comida}</a>
+        </h4>
+        <h5>$${precioXUnidad}</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!
+        </p>
+      </div>
+
+    </div>
+  </div>
+  `;
+    contenedorDeProdutcotos.append(texXunidad);
+  });
+}
+function FiltrosDeCategoria() {
+  querySnapshot.forEach((doc) => {
+    var categoria1 = doc.data().categoria1;
+    var categoria2 = doc.data().categoria2;
+    arrayDeCategoria1.push(categoria1);
+    arrayDeCategoria2.push(categoria2);
+  });
+  arrayDeCategoria2.unique().forEach((e) => {
+    $("#categorias")
+      .append(`<a class="list-group-item categoriaselect" data-categoria2="${e}">${e}</a>
+    `);
+  });
+  var catergorias = document.querySelectorAll('.categoriaselect')
+  console.log(catergorias);
+  
+  catergorias.forEach(function (item) {
+    item.addEventListener('click', function () {FiltrarCategoria()
+      categoriaseleccionada = item.dataset.categoria2})
+  })
+}
+
+
+function FiltrarCategoria() {
+  console.log(categoriaseleccionada);
+      $("#productos").empty()
+
+      querySnapshot.forEach((doc) => {
+        contenedorDeProdutcotos = $("#productos");
+        var comida = doc.data().name;
+        var categoria2 = doc.data().categoria2;
+        var precioXUnidad = doc.data().precioXUnidad;
+        var precioXDocena = doc.data().precioXDocena;
+        var img = doc.data().img;
+        var texXunidad = `
         <div class="col-lg-4 col-md-6 mb-4">
         <div class="card h-100">
           <a href="#"><img class="card-img-top" src="${img}" alt="img"></a>
@@ -52,25 +105,15 @@ window.addEventListener('DOMContentLoaded', async (e) => {
             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!
             </p>
           </div>
-
+    
         </div>
       </div>
       `;
-      arrayDeCategoria1.push(categoria1)
-      arrayDeCategoria2.push(categoria2)
-    contenedorDeProdutcotos.append(texXunidad);
-    })
-    arrayDeCategoria2.unique().forEach(e =>{
-        $('#categorias').append(`<a href="Agregar productos a base de datos(soloComidas).html" class="list-group-item">${e}</a>
-        `);
-    })
-
-
-    console.log(comida
-      ,categoria1
-      ,categoria2
-      ,precioXUnidad
-      ,precioXDocena
-      ,img,arrayDeCategoria2.unique(),$('#categorias')
-      )
-  })
+      if (categoriaseleccionada == categoria2 ){
+        contenedorDeProdutcotos.append(texXunidad);
+      }
+      else if(categoriaseleccionada== 'all' ){
+        contenedorDeProdutcotos.append(texXunidad);
+      }
+      });
+}
